365bb4a9e1bf1277160725abced2cb777f3401ec
fjrjdev <thedromemafia@gmail.com>
2022-10-28 18:32:35 -0300

    fix: logo name

--- src/components/ResposiveAppBar/index.jsx
+++ src/components/ResposiveAppBar/index.jsx
@@ -62,7 +62,7 @@ const ResposiveAppBar = () => {
               cursor: "pointer",
             }}
           >
-            Kenzie Music
+            Music
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
